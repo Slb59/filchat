f39a10fce25074e417f7f004385f1de402dff93f
Osynia <112859070+Slb59@users.noreply.github.com>
2026-01-22 21:41:13 +0100

    ajout de la lecture des variables d'environnement

---
+++ config/__init__.py
@@ -0,0 +1,20 @@
+import pathlib
+import environ  # type: ignore
+
+BASE_DIR = pathlib.Path(__file__).resolve().parent.parent
+
+env = environ.Env(
+    DEBUG=(bool, False),
+)
+
+env.read_env(str(BASE_DIR / '.env'))
+
+
+VERSION_FILE = BASE_DIR / "VERSION"
+with open(VERSION_FILE, "r") as f:
+    VERSION = f.read().strip()
+
+
+def get_version():
+    """Retourne la version de l'application"""
+    return VERSION
---
+++ makefile
@@ -0,0 +1,3 @@
+run:
+	clear
+	uv run manage.py runserver
